a07f46ebfe20b53fd8d9cabc81bcd2b2d36643bc
Miroslav Suchý <miroslav@suchy.cz>
2016-03-21 22:40:16 +0100

    prvni nastrel

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# Programování_pro_deti
+# Programování pro děti
 Příručka pro děti a rodiče o výuce programování dětí na prvním stupni (tj. věk 6 až 11 let).
+
+## O příručce
+
+Tento text není a nebude učebnicí programovaní a není pro děti. Slouží pro rodiče, vedoucí programátorských kroužků a pro učitele informatiky. Je o tom jak učit malé děti programovat, jak je naučit algoritmizovat a pochopit základní programátorské dovednosti. Není to ani příručka, podle které by se dalo postupovat krok za krokem, protože každé dítě a každá třída je vysoce individuální. Zkusím zde popsat různé možnosti výuky a jejich kombinace a uvést nezbytné detaily, které vám umožní vytvořit si vlastní učební plán. Jedná se zkušenosti mé a zkušenosti dalších učitelů a rodičů. A samozřejmě uvítám, pokud se se mnou podělíte o svoje zkušenosti. Můžete mi psát na miroslav@suchy.cz.
+
+Nemám potřebu vytvářet nějaký nový učební systém na programovaní. Už jich existuje plno. Budu zde na ně uvádět odkazy. A budu je také odstraňovat tak jak budou některé z nich zanikat a jiné místo nich vznikat. Pokud naleznete nějaký skvělý výukový program, tak budu rád když mi dáte vědět.
+
+Tento text je zaměřen na děti v prvních ročnících základních škol. Pokud hledáte příručku pro střední školy a případně poslední ročníky základních škol tak vám doporučuji [Informatiku pro každého](http://popelka.ms.mff.cuni.cz/~lessner/mw/index.php/U%C4%8Debnice/%C3%9Avod/P%C5%99edmluva_pro_u%C4%8Ditele), která je pro starší děti vhodnější.
+
 
 # Licence
 
---
+++ myslenkova-mapa.mm
@@ -0,0 +1,367 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1450271018685" ID="ID_1071336423" MODIFIED="1450469144398" TEXT="Programov&#xe1;n&#xed; pro d&#x11b;ti">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      33 tydnu
+    </p>
+    <p>
+      https://vyzkumnik.seznam.cz/zpravodaj/jake-jsou-internetove-deti-1-co-si-odnesou-z-hodin-informatiky
+    </p>
+    <p>
+      http://wvc.pf.jcu.cz/ki/?article=/katedra/o-katedre.html
+    </p>
+    <p>
+      http://www.petrinyjih.cz/stranka-rozsirena-vyuka-informatiky-66
+    </p>
+    <p>
+      http://www.ceskaskola.cz/2015/12/e-kniha-pro-vas-vyuka-zakladu.html
+    </p>
+    <p>
+      http://popelka.ms.mff.cuni.cz/~lessner/mw/index.php/U%C4%8Debnice/%C3%9Avod/P%C5%99edmluva_pro_u%C4%8Ditele
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1450271078908" ID="ID_985409642" MODIFIED="1450271095711" POSITION="right" TEXT="http://www.progetiiger.ee/">
+<node CREATED="1450271100689" ID="ID_420309428" MODIFIED="1450271104772" TEXT="Estonsko"/>
+<node CREATED="1450271109922" ID="ID_544670532" MODIFIED="1450271168735" TEXT="Tieto z Finska"/>
+</node>
+<node CREATED="1450271171800" FOLDED="true" ID="ID_1506595644" MODIFIED="1450360581688" POSITION="left" TEXT="GCompris">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Po&#269;&#237;tejte s &#269;asovou dotac&#237; 8 minut na jednu lekci. Tj. max. 5 lekc&#237; b&#283;hem jedn&#233; vyu&#269;ovac&#237; hodiny.
+    </p>
+    <p>
+      V&#382;dy si zopakujte dv&#283; star&#353;&#237; lekce z p&#345;edchoz&#237; hodiny (dle vlastn&#237;ho uv&#225;&#382;en&#237;) a 3 nov&#233;.
+    </p>
+    <p>
+      GCompris je mo&#382;n&#233; vyu&#382;&#237;t i kdykoliv pozd&#283;ji p&#345;i v&#253;uce programov&#225;n&#237; - kdy&#382; &#269;ek&#225;te a&#382; p&#345;ijdou v&#353;ichni &#382;&#225;ci nebo kdy&#382; &#382;&#225;k skon&#269;&#237; d&#345;&#237;ve tak jim m&#367;&#382;ete &#345;&#237;ci, &#382;e si m&#367;&#382;ou zahr&#225;t n&#283;co z tu&#269;n&#225;ka (ikona programu GCompris) dle vlastn&#237; volby.
+    </p>
+    <p>
+      Pokryt&#237; v&#353;ech vybran&#253;ch lekc&#237; z GComprisu byste m&#283;li zvl&#225;dnout za 4,5 vyu&#269;ovac&#237; hodiny.&#160;
+    </p>
+    <p>
+      Ke sta&#382;en&#237; pro Windows, Linux a Mac.
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-1"/>
+<node CREATED="1450274130890" FOLDED="true" ID="ID_1998368434" MODIFIED="1450360386117" TEXT="1. Prace s mysi">
+<node CREATED="1450272874044" ID="ID_1038114249" MODIFIED="1450274549160" TEXT="1. Hybej my&#x161;&#xed;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kocicka &gt; mys &gt; hybej mysi
+    </p>
+    <p>
+      Musi prejet mysi pres celou plochu obrazovky.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450274150011" ID="ID_1513331511" MODIFIED="1450274511896" TEXT="2. Klikej my&#x161;&#xed;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kocicka &gt; mys &gt; klikej mysi
+    </p>
+    <p>
+      kliknutim odkryjes pozadi
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450274330580" ID="ID_1301803632" MODIFIED="1450274499177" TEXT="3. Klikn&#x11b;te a kreslete">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kocicka &gt; mys &gt; kliknete a kreslete
+    </p>
+    <p>
+      spojovani bodu, presne klikani
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450274573447" ID="ID_1476988390" MODIFIED="1450274758625" TEXT="4. Ovll&#xe1;dej hadici">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kocicka &gt; mys &gt; Ovl&#225;dej hadici
+    </p>
+    <p>
+      p&#345;esn&#233; t&#225;hnut&#237; my&#353;i po dr&#225;ze
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450274772533" ID="ID_831411043" MODIFIED="1450274878633" TEXT="5. Dvojit&#xe9; klik&#xe1;n&#xed; my&#x161;&#xed;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kocicka &gt; mys &gt; dvojit&#233; klik&#225;n&#237; my&#353;&#237;
+    </p>
+    <p>
+      n&#225;cvik dvojkliku
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450275106299" ID="ID_166511350" MODIFIED="1450275213983" TEXT="6. Dolov&#xe1;n&#xed; zlata">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kocicka &gt; mys &gt; Dolov&#225;n&#237; zlata
+    </p>
+    <p>
+      procvi&#269;en&#237; skrolovac&#237;ho kole&#269;ka
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1450275251583" FOLDED="true" ID="ID_586358673" MODIFIED="1450360386117" TEXT="2. Pr&#xe1;ce s kl&#xe1;vesnic&#xed;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ko&#269;ka &gt; kl&#225;vesnice
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1450275549318" ID="ID_1794294938" MODIFIED="1450275653995" TEXT="1. Jednotliv&#xe1; p&#xed;smena">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ko&#269;ka &gt; kl&#225;vesnice &gt; Jednotliv&#225; p&#237;smena
+    </p>
+    <p>
+      z&#225;klady orientace na alfanumerick&#233; kl&#225;vesnici
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450275626678" ID="ID_990120205" MODIFIED="1450275645720" TEXT="2. &#x10d;&#xed;sla na kostce">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ko&#269;ka &gt; kl&#225;vesnice &gt; Jednotliv&#225; p&#237;smena
+    </p>
+    <p>
+      z&#225;klady orientace na numerick&#233; kl&#225;vesnici
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1450359178362" FOLDED="true" ID="ID_1815799744" MODIFIED="1450360386117" TEXT="3. Pam&#x11b;tov&#xe9; aktivity">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      tu&#269;&#328;&#225;k &gt; slon
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1450359194285" ID="ID_1958671527" MODIFIED="1450359446344" TEXT="1. &#x17e;eleznice">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      tu&#269;&#328;&#225;k &gt; slon &gt; ma&#353;inka
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1450359447781" FOLDED="true" ID="ID_142251816" MODIFIED="1450360386117" TEXT="4. Prostorov&#xe1; orientace">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      tu&#269;&#328;&#225;k &gt; bludi&#353;t&#283;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1450359467134" ID="ID_1329234525" MODIFIED="1450359517174" TEXT="1. bludi&#x161;t&#x11b;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      tu&#269;&#328;&#225;k &gt; bludi&#353;t&#283; &gt; bludi&#353;t&#283;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450359519791" ID="ID_229836138" MODIFIED="1450359639852" TEXT="2. bludi&#x161;t&#x11b; (relativn&#xed; pohyb)">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      tu&#269;&#328;&#225;k &gt; bludi&#353;t&#283; &gt; bludi&#353;t&#283; (relativn&#237;)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1450359668405" FOLDED="true" ID="ID_1485846038" MODIFIED="1450360386118" TEXT="5. algoritmy">
+<node CREATED="1450359679834" ID="ID_1350569613" MODIFIED="1450359759857" TEXT="1. pr&#x16f;jezd plavebn&#xed; komorou">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      pras&#225;tko &gt; lodi&#269;ka s kruhem
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450359792406" ID="ID_305727515" MODIFIED="1450359879861" TEXT="2. &#x161;esti&#xfa;heln&#xed;k">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      medv&#283;d &gt; &#353;esti&#250;heln&#237;k
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450359973929" ID="ID_131049806" MODIFIED="1450360128220" TEXT="3. s&#x10d;&#xed;t&#xe1;n&#xed;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ove&#269;ka &gt; tabule s k&#345;&#237;dou &gt; s&#269;&#237;t&#225;n&#237;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450360129324" ID="ID_1959841291" MODIFIED="1450360227175" TEXT="4. posouv&#xe1;n&#xed; blok&#x16f;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      panda &gt; aut&#237;&#269;ko
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1450271186087" ID="ID_1346886006" MODIFIED="1450367846992" POSITION="right" TEXT="Scratch">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Verze 1.4 ke sta&#382;en&#237;
+    </p>
+    <p>
+      https://scratch.mit.edu/scratch_1.4/
+    </p>
+    <p>
+      pro Linux, Mac OS a Windows
+    </p>
+    <p>
+      Verze 2 je webov&#225; ale je i offline klient pro Windows, Mac OS a 32 bitov&#253; Linux
+    </p>
+    <p>
+      https://scratch.mit.edu/
+    </p>
+    <p>
+      http://scratched.gse.harvard.edu/resources/education-basic-programming-scratch-environment
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-2"/>
+<node CREATED="1450272745559" ID="ID_732450282" MODIFIED="1450367849837" TEXT=""/>
+</node>
+<node CREATED="1450271381116" ID="ID_136187812" MODIFIED="1450272811779" POSITION="left" TEXT="Make games with Python">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      http://www.root.cz/knihy/make-games-with-python/
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1450272704158" ID="ID_820939593" MODIFIED="1450272709687" POSITION="right" TEXT="LightBot"/>
+</node>
+</map>
